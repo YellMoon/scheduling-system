76611c291ca9962ec6bd927b4cdb9e886c6dbf0e
YellMoon <yellmoon@users.noreply.github.com>
2026-06-12 04:32:57 +0800

    fix: clamp data drawer width

--- src/layout/DataPageLayout.tsx
+++ src/layout/DataPageLayout.tsx
@@ -25,26 +25,33 @@ const DataPageLayout: React.FC<DataPageLayoutProps> = ({
   drawerExtra,
   drawerFooter,
   destroyOnClose,
-}) => (
-  <div className="data-page-layout">
-    <Card className="data-page-layout__toolbar" size="small">
-      {toolbar}
-    </Card>
-    <Card className="data-page-layout__table" size="small">
-      {table}
-    </Card>
-    <Drawer
-      title={drawerTitle}
-      open={drawerOpen}
-      onClose={onDrawerClose}
-      width={drawerWidth}
-      extra={drawerExtra}
-      footer={drawerFooter}
-      destroyOnClose={destroyOnClose}
-    >
-      {drawerContent}
-    </Drawer>
-  </div>
-);
+}) => {
+  const responsiveDrawerWidth =
+    typeof drawerWidth === 'number'
+      ? `min(${drawerWidth}px, calc(100vw - 16px))`
+      : drawerWidth;
+
+  return (
+    <div className="data-page-layout">
+      <Card className="data-page-layout__toolbar" size="small">
+        {toolbar}
+      </Card>
+      <Card className="data-page-layout__table" size="small">
+        {table}
+      </Card>
+      <Drawer
+        title={drawerTitle}
+        open={drawerOpen}
+        onClose={onDrawerClose}
+        width={responsiveDrawerWidth}
+        extra={drawerExtra}
+        footer={drawerFooter}
+        destroyOnClose={destroyOnClose}
+      >
+        {drawerContent}
+      </Drawer>
+    </div>
+  );
+};
 
 export default DataPageLayout;
