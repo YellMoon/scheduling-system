912e18260d1b690e6dc0b6eef67ba5977ef9311b
YellMoon <yellmoon@users.noreply.github.com>
2026-06-25 15:19:55 +0800

    自动发布 2026-06-25

--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated - do not edit
-// Updated: 2026-06-25T06:57:35.998Z
-// Build: 20260625-1457
-export const APP_VERSION = "5.0.28";
-export const BUILD_TAG = "20260625-1457";
+// Updated: 2026-06-25T07:14:52.829Z
+// Build: 20260625-1514
+export const APP_VERSION = "5.0.29";
+export const BUILD_TAG = "20260625-1514";
